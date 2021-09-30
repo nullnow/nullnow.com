9bf8467553434d3bc7b7e7f56f3bb788718a7b34
Ninte <ninte.dev@gmail.com>
2021-09-30 09:26:30 +0100

    Updated mission statement.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import "./App.css";
+import './App.css';
 
-import Navbar from "./main/Navbar";
-import Description from "./main/sections/Description";
-import OurVision from "./main/sections/Vision";
-import TheMission from "./main/sections/Mission";
-import Contact from "./main/sections/Contact";
-import Copyright from "./main/sections/Copyright";
+import Navbar from './main/Navbar';
+import Description from './main/sections/Description';
+import OurVision from './main/sections/Vision';
+import TheMission from './main/sections/Mission';
+import Contact from './main/sections/Contact';
+import Copyright from './main/sections/Copyright';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <h3 style={{ color: '#639', margin: '50px 0px 20px 0' }}>Building digital tools for creative projects.</h3>
+        <h3 style={{ color: '#639', margin: '50px 0px 20px 0' }}>
+          Building a digital feed for marketing content.
+        </h3>
         <Description />
         <OurVision />
         <TheMission />

e4de967eec0f35d85adf18b16a30de5d5a304014
Ninte <ninte.dev@gmail.com>
2021-12-03 19:36:34 +0100

    Modified layout.

--- src/main/Display.js
+++ src/main/Display.js
@@ -1,39 +1,50 @@
 import React, { useState } from 'react';
 
-function Info({ name, info }) {
-  const [message, setMessage] = useState('');
+function Info({ name, message }) {
+  const [show, setShow] = useState('hidden');
 
-  const clicker = () => (message === '' ? setMessage(info) : setMessage(''));
+  const output = () =>
+    show === 'hidden' ? setShow('visible') : setShow('hidden');
 
   return (
-    <div className="info-base" onClick={clicker}>
-      <div className="info-box">
+    <div className="info-base" onClick={output}>
+      <span className="info-box">
         <h4>{name}</h4>
         <i class="bi bi-arrow-bar-down special-button-icon-alt"></i>
-      </div>
-      <p className="info-text">{message}</p>
+      </span>
+      <span className="info-text" style={{ visibility: show }}>
+        {message}
+      </span>
     </div>
   );
 }
 
 export default function Display() {
-  return (
-    <div>
-      <div className="information">
-        <Info
-          name="Description"
-          info="Nullnow Computing is a business technology partner for small-to-medium enterprises within the African continental region. The impact of technical innovation on societal improvements and profit does not escape us. We intend to become an established engine of growth and excellence."
-        />
-        <Info
-          name="The Mission"
-          info="To serve as an entity of continental growth and empowerment - championing efficiency and excellence, at every turn. To improve the way businesses grow and scale, one stakeholder at a time, each day."
-        />
+  const input = [
+    {
+      header: 'who we are',
+      value:
+        'Nullnow Computing is a business technology partner for small-to-medium enterprises within the African continental region. The impact of technical innovation on societal improvements and profit does not escape us. We intend to become an established engine of growth and excellence.',
+    },
+    {
+      header: 'the mission',
+      value:
+        'To serve as an entity of continental growth and empowerment - championing efficiency and excellence, at every turn. To improve the way businesses grow and scale, one stakeholder at a time, each day.',
+    },
+    {
+      header: 'our vision',
+      value:
+        'To impact positive change even as we change the way businesses industrialize in Africa. Doing extraordinary things in ordinary ways.',
+    },
+  ];
 
-        <Info
-          name="Our Vision"
-          info="To impact positive change even as we change the way businesses industrialize in Africa. Doing extraordinary things in ordinary ways."
-        />
-      </div>
+  return (
+    <div className="information">
+      {input.map((item) => {
+        return (
+          <Info name={item['header'].toUpperCase()} message={item['value']} />
+        );
+      })}
     </div>
   );
 }

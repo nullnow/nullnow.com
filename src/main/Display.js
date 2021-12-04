c204f2c435541842f9adfd544fdf8a07d462c14b
Ninte <ninte.dev@gmail.com>
2021-12-04 10:28:57 +0100

    Updated display functionality.

--- src/main/Display.js
+++ src/main/Display.js
@@ -1,20 +1,13 @@
 import React, { useState } from 'react';
 
 function Info({ name, message }) {
-  const [show, setShow] = useState('hidden');
-
-  const output = () =>
-    show === 'hidden' ? setShow('visible') : setShow('hidden');
-
   return (
-    <div className="info-base" onClick={output}>
+    <div className="info-base">
       <span className="info-box">
-        <h4>{name}</h4>
-        <i class="bi bi-arrow-bar-down special-button-icon-alt"></i>
+        <h1>{name}</h1>
       </span>
-      <span className="info-text" style={{ visibility: show }}>
-        {message}
-      </span>
+
+      <span className="info-text">{message}</span>
     </div>
   );
 }
@@ -27,14 +20,9 @@ export default function Display() {
         'Nullnow Computing is a business technology partner for small-to-medium enterprises within the African continental region. The impact of technical innovation on societal improvements and profit does not escape us. We intend to become an established engine of growth and excellence.',
     },
     {
-      header: 'the mission',
+      header: 'mission & vision',
       value:
-        'To serve as an entity of continental growth and empowerment - championing efficiency and excellence, at every turn. To improve the way businesses grow and scale, one stakeholder at a time, each day.',
-    },
-    {
-      header: 'our vision',
-      value:
-        'To impact positive change even as we change the way businesses industrialize in Africa. Doing extraordinary things in ordinary ways.',
+        'We serve as an entity of continental growth and empowerment - championing efficiency and excellence, at every turn. We are positioned to improve the way businesses grow and scale, one stakeholder at a time, each day. Our vision is to change the way businesses industrialize in Africa, by doing extraordinary things in ordinary ways.',
     },
   ];
 
